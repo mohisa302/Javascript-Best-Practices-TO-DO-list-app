e07d0249bf789e47ae7aa557a95f5895dd9ba046
mohi.sa <mohisadeghi302@gmail.com>
2023-05-31 18:53:13 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './style.css';
 import myTaskList from './modules/TaskList.js';
 import populateTasks from './modules/populateTasks.js';
+import './style.css';
 
 window.onload = () => {
   const storedTasks = JSON.parse(window.localStorage.getItem('storedTasks'));
