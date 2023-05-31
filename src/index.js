535e192357508d212940c1c3ee66d12cd16fa3ca
mohi.sa <mohisadeghi302@gmail.com>
2023-05-31 18:38:07 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './style.css';
 import myTaskList from './modules/TaskList.js';
 import populateTasks from './modules/populateTasks.js';
-
 window.onload = () => {
   const storedTasks = JSON.parse(window.localStorage.getItem('storedTasks'));
-  if (storedTasks) {
-    myTaskList.tasks = storedTasks;
-  }
+  myTaskList.tasks = storedTasks || myTaskList.tasks;
   populateTasks();
 };
